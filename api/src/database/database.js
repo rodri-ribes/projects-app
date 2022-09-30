33cf5f5b53eec304b579e81c9f0fc76f15f3e576
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-09-30 20:53:48 -0300

    asdf

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,6 +1,7 @@
 import Sequelize from "sequelize";
 // import * as dotenv from 'dotenv'
 // dotenv.config()
+import pg from 'pg';
 
 
 export const sequelize = new Sequelize("postgresql://postgres:tveE3y5oW6aREykN39ta@containers-us-west-82.railway.app:6033/railway", {
