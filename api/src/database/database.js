94946cff6394980e16ae6270baab559664ce457f
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-09-30 20:28:08 -0300

    adsad

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -5,10 +5,10 @@ import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize("postgresql://postgres:tveE3y5oW6aREykN39ta@containers-us-west-82.railway.app:6033/railway", {
     logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+    // dialectOptions: {
+    //     ssl: {
+    //         require: true,
+    //         rejectUnauthorized: false
+    //     }
+    // }
 })
