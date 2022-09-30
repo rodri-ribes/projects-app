8b19959cb957db254797599f6cc55b68c9b4db36
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-09-30 20:22:37 -0300

    first commit

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,9 +1,9 @@
 import Sequelize from "sequelize";
-import * as dotenv from 'dotenv'
-dotenv.config()
+// import * as dotenv from 'dotenv'
+// dotenv.config()
 
 
-export const sequelize = new Sequelize(process.env.URL_POSTGRESQL, {
+export const sequelize = new Sequelize("postgresql://postgres:tveE3y5oW6aREykN39ta@containers-us-west-82.railway.app:6033/railway", {
     logging: false,
     dialectOptions: {
         ssl: {
