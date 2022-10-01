c29e0b7070a6659ead0255a528a9cbd06368d41a
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-09-30 21:14:42 -0300

    first commit

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -6,11 +6,5 @@ import pg from 'pg';
 
 export const sequelize = new Sequelize(process.env.URL_POSTGRESQL, {
     logging: false,
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // },
     dialectModule: pg
 })
--- api/src/routes/projects.routes.js
+++ api/src/routes/projects.routes.js
@@ -5,6 +5,7 @@ import verifyToken from "./verifyToken.js";
 
 const router = Router();
 
+
 router.get('/project/:id', verifyToken, getProject)
 router.get('/projects/:id', verifyToken, getProjects)
 router.post('/projects', verifyToken, createProject)
