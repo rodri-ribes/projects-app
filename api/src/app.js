b41ea32ea7e461771c96c605ae961f0336049dde
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-11-18 19:34:25 -0300

    first commit

--- api/src/app.js
+++ api/src/app.js
@@ -9,6 +9,14 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
+
 app.use(projectsRoutes);
 app.use(taskRoutes);
 app.use(usersRoutes);
